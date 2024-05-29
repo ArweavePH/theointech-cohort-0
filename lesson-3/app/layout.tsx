d24260c715fb3eb7c6bf229ccc2f8453ff4baeb4
Theo in Tech <theoroque95@gmail.com>
2024-05-29 19:33:03 +0800

    feat: create a new next app for lesson 3

---
+++ lesson-3/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Arweave Philippines | Lesson 3",
+  description:
+    "Arweave Philippines Lesson 3, uploading a static site to Arweave.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ lesson-3/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
+      <h1 className="text-4xl">Hello, world!</h1>
+      <h2 className="text-2xl">
+        I&apos;m TheoInTech and we are Arweave Philippines!
+      </h2>
+    </main>
+  );
+}
