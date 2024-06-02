c4cf2aa288eade676d38d68bfc3a7a5bb7630bd2
Theo in Tech <theoroque95@gmail.com>
2024-06-02 23:33:26 +0800

    push lesson 3

--- lesson-3/app/page.tsx
+++ lesson-3/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-      <h1 className="text-4xl">Hello, world!</h1>
-      <h2 className="text-2xl">
-        I&apos;m TheoInTech and we are Arweave Philippines!
-      </h2>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl">Hello world! From Arweave Philippines! 🇵🇭</h1>
     </main>
   );
 }
